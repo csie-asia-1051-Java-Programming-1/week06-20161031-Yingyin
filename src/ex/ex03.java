d674b1a9de2dbc7d3448d04e9c17ced9707c1113
Yingyin <105791120@live.asia.edu.tw>
2016-10-31 15:40:36 +0800

    ex03 105791120 杨钰莹

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -1,4 +1,7 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 輸入一串數字，寫出一數字拆解函數decomp(n)，將其個別數字用空白分開。例如:輸入128917178, 則輸出1 2 8 9 1 7 1 7 8
  * Date: 2016/10/31
@@ -8,8 +11,17 @@
 public class ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sca=new Scanner(System.in);
+	String n=sca.nextLine();
+	decomp(n);
+	}
 
+	private static void decomp(String n) {
+	char arr[]=n.toCharArray();
+	for(int i=0;i<arr.length;i++)
+	{
+		System.out.print(arr[i]+" ");
+	}
 	}
 
 }
