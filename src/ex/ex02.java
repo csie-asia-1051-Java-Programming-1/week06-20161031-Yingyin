e97d235ef6c95283ed38762770119bf2c4314406
Yingyin <105791120@live.asia.edu.tw>
2016-10-31 15:40:16 +0800

    ex02 105791120 杨钰莹

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,4 +1,7 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 使用者輸入兩個整數，透過函數呼叫輸出排列組合函數 C(m,n)。公式: C(m,n) = m!/n!(m-n)!
  * Date: 2016/10/31
@@ -8,8 +11,23 @@
 public class ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sca =  new Scanner(System.in);
+	int m=sca.nextInt();
+	int n=sca.nextInt();
+	System.out.println(C(m,n));
+	}
 
+	private static int C(int m, int n) {
+	int temp1=1,temp2=2;
+	for(int i=m-n+1;i<m;i++)
+	{
+		temp1*=i;
+	}
+	for(int i=1;i<n;i++)
+	{
+		temp2*=i;
+	}
+		return temp1/temp2;
 	}
 
 }
