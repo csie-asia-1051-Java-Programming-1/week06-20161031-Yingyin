b2851c64ab2ad68e167d20f5966b2defed5fd0f9
Yingyin <105791120@live.asia.edu.tw>
2016-11-06 17:21:31 +0800

    hw01 105791120 杨钰莹

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,33 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sca = new Scanner(System.in);
+	int type;
+	float V;
+	V=sca.nextFloat();
+	type=sca.nextInt();
+	if(type==1)
+	{
+		System.out.println(C(V));
+	}
+	else if(type==2)
+	{
+		System.out.println(F(V));
+	}
+	
+	}
 
+	private static float F(float c) {
+		
+		return (float)(c*9.0/5.0+32.0);
+	}
+
+	private static float C(float f) {
+		return (float)((f-32.0)*(5.0/9.0));
 	}
 
 }
