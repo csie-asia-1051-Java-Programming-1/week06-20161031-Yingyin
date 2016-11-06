66c3891c8dd406cc6a6e128ee6e996fdafd9e59b
Yingyin <105791120@live.asia.edu.tw>
2016-11-06 17:27:37 +0800

    hw02 105791120 杨钰莹

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,31 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca = new Scanner(System.in);
+	int type=sca.nextInt();
+	float height=sca.nextFloat();
+	if(type==1)
+	{
+		System.out.println(man(height));
+	}else if(type==2)
+	{
+		System.out.println(femle(height));
+	}
+	
+	}
+	
+   private static float man(float height) {
+		return (float)((height-170)*0.6+62) ;
 	}
 
+
+
+	private static float femle(float height) {
+		return (float)((height-158)*0.5+52);
+	}
 }
+	
